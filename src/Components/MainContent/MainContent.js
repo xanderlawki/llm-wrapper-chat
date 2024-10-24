ee8b53c69c8a387a34eab92c40dea4e978dc8374
daudalu <lucky.dauda@tecnotree.com>
2024-10-24 02:01:16 +0100

    fix build issues

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -1,6 +1,8 @@
 import Chatbox from "../Chatbox/Chatbox";
 import Header from "../Header/Header";
-import Navbar from "../NavBar/navbar";
+
+import Navbar from "../NavBar/Navbar";
+
 import styles from "./styles.module.css";
 
 const MainContent = () => {
