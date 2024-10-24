45d0b19315f30c9c3e877818535b16431a961a6c
daudalu <lucky.dauda@tecnotree.com>
2024-10-24 01:40:32 +0100

    fix css issues on the main content

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
     NEXT_PUBLIC_GOOGLE_AUTH_URL: process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL,
+    NEXT_SCRAPPER_API_KEY: process.env.NEXT_SCRAPPER_API_KEY,
   },
 };
 
