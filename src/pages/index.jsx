82f86f2fd6e95b60b193a1cc496ee4a20eab20b4
daudalu <lucky.dauda@tecnotree.com>
2024-10-24 02:21:14 +0100

    fix build issues

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,16 +1,9 @@
-import MainContent from "@/components/MainContent/MainContent";
-import Sidebar from "@/components/Sidebar/Sidebar";
-
 export default function Home() {
   return (
     <div
       style={{ position: "relative", flexDirection: "row", display: "flex" }}
     >
-      <div>
-        <Sidebar />
-      </div>
-
-      <MainContent />
+      hey
     </div>
   );
 }
