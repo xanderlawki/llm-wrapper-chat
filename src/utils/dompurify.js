8422911ff617d944b6323a560b50cd6e0b14b7ce
daudalu <lucky.dauda@tecnotree.com>
2024-10-26 01:59:51 +0100

    remove unused codes

--- src/utils/dompurify.js
+++
@@ -1,6 +0,0 @@
-import DOMPurify from "dompurify";
-
-export const cleanResponse = (rawHtml) => {
-  const cleanHtml = DOMPurify.sanitize(rawHtml); // This will remove unwanted tags/attributes
-  return cleanHtml;
-};
